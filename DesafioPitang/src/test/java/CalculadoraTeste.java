a34729875401d55d5fc09f8746704752332b89e1
Cesar Augusto Silva de Araujo <cesar.araujo@mv.com.br>
2023-10-21 20:53:36 -0300

    Atualização de arquivos

--- DesafioPitang/src/test/java/CalculadoraTeste.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-
-public class CalculadoraTeste {
-    @Test
-    public void testSoma(){
-        Calculadora c = new Calculadora(  2,  3);
-        assertEquals(5.0,c.soma(),0.001);
-    }
-}
